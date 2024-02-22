5b8519a8f5f2c95fa8b5d94704ede137789a5ec0
Phumtummaruk <Phumtummaruk@hotmail.com>
2024-02-23 00:27:17 +0100

    First task finished

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using HTTPUtils;
+using System.Text.Json;
+using AnsiTools;
+using Colors = AnsiTools.ANSICodes.Colors;
+
+Console.Clear();
+Console.WriteLine("Starting Assignment 2");
+
+// SETUP 
+const string myPersonalID = "3f0cd06c12293f7c1721728508095628e85e55fe0bd5d61ee8870d6e03a4bf66"; // GET YOUR PERSONAL ID FROM THE ASSIGNMENT PAGE https://mm-203-module-2-server.onrender.com/
+const string baseURL = "https://mm-203-module-2-server.onrender.com/";
+const string startEndpoint = "start/"; // baseURl + startEndpoint + myPersonalID
+const string taskEndpoint = "task/";   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
+// Creating a variable for the HttpUtils so that we dont have to type HttpUtils.instance every time we want to use it
+HttpUtils httpUtils = HttpUtils.instance;
+
+//#### REGISTRATION
+// We start by registering and getting the first task
+Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
+Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
+string taskID = "psu31_4"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+
+//#### FIRST TASK 
+// Fetch the details of the task from the server.
+Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Console.WriteLine(task1Response);
+
+Task task1 = JsonSerializer.Deserialize<Task>(task1Response.content);
+Console.WriteLine($"TASK1: {ANSICodes.Effects.Bold}{task1?.title}{ANSICodes.Reset}\n{task1?.description}\nParameters: {Colors.Yellow}{task1?.parameters}{ANSICodes.Reset}");
+
+var answer = task1?.parameters.Split(",").Select(p => int.Parse(p.Trim())).Aggregate<int, int>(0, (acc, n) => acc + n);
+
+
+Response task1AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answer.ToString());
+Console.WriteLine($"Answer: {Colors.Green}{answer}\n {task1AnswerResponse}{ANSICodes.Reset}");
+
+
+
+
+
+class Task
+{
+    public string? title { get; set; }
+    public string? description { get; set; }
+    public string? taskID { get; set; }
+    public string? usierID { get; set; }
+    public string? parameters { get; set; }
+}
